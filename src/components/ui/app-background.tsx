6a5ceb4c7f49e773aaf94ecef22f09c0608e885a
Hussain <shaikhhussi786@gmail.com>
2026-06-30 14:54:26 +0530

    done

--- src/components/ui/app-background.tsx
+++ src/components/ui/app-background.tsx
@@ -2,7 +2,7 @@
 export function Appbackground(){
     return(
     <div className="pointer-events-none fixed inset-0 -z-10">
-      <div className="absolute inset-0 bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950" />
+      <div className="absolute inset-0 bg-linear-to-br from-slate-950 via-slate-900 to-slate-950" />
     </div>
     );
 }
